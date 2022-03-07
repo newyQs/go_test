2ff192f238fa603a99ee1043bafedddcc6b34648
admin <123@qq.com>
2022-03-07 19:16:20 +0800

    'runoob'

---
+++ runoob/02 Go 基础语法/01 行分隔符.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+	Go 程序可以由多个标记组成，可以是关键字，标识符，常量，字符串，符号。如以下 GO 语句由 6 个标记组成：
+	fmt.Println("Hello, World!")
+	1. fmt
+	2. .
+	3. Println
+	4. (
+	5. "Hello, World!"
+	6. )
+
+	标识符用来命名变量、类型等程序实体。
+	一个标识符实际上就是一个或是多个字母(A~Z和a~z)数字(0~9)、下划线_组成的序列，但是第一个字符必须是字母或下划线而不能是数字。
+	*/
+	fmt.Println("Hello, World!")
+	fmt.Println("菜鸟教程：runoob.com")
+}
--- runoob/02 Go 基础语法/行分隔符.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println("菜鸟教程：runoob.com")
-}
